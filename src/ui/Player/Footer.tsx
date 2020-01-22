a7adb1f67896abcc5f9d69f46d26a90f00aed88a
danigb <danigb@gmail.com>
2020-01-22 20:15:05 +0100

    Fix about

--- src/ui/Player/Footer.tsx
+++ src/ui/Player/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { FullscreenIcon, Stop } from "../shared/Icons";
+import { Link } from "react-router-dom";
+import { FullscreenIcon, Info, Stop } from "../shared/Icons";
 
 interface FooterProps {
   onStopAll: () => void;
@@ -9,7 +10,11 @@ interface FooterProps {
 export const Footer = ({ onFullscreen, onStopAll }: FooterProps) => {
   return (
     <footer className="Footer">
-      <div className="left">...</div>
+      <div className="left">
+        <Link to="/about">
+          <Info />
+        </Link>
+      </div>
       <div className="center" />
       <div className="right">
         <button className="btn-link" title="Full screen" onClick={onFullscreen}>
