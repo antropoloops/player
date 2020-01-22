1c091ade1cae3f92bcaf10da81f6c931a9548168
danigb <danigb@gmail.com>
2020-01-22 20:16:21 +0100

    About page (#43)

--- src/ui/App/About.tsx
+++ src/ui/App/About.tsx
@@ -4,29 +4,42 @@ import { Header } from "../shared/Header";
 import { Markdown } from "../shared/Markdown";
 import { Scroll } from "../shared/Scroll";
 
-const VERSION = "3.4.0";
+const VERSION = "3.4.1";
 const BODY = `
-<h1>Antropoloops Player</h1>
 <p>Versión ${VERSION}</p>
 <p>
-Antropoloops Player es una herramienta
+play/antropoloops es una herramienta
 desarrollada dentro del contexto de los
-<a href="https://talleres.antropoloops.com">Talleres Antropoloops</a>
+<a target="_blank" href="https://talleres.antropoloops.com">Talleres Antropoloops</a>
 </p>
 <p>
-Ha sido desarrollada gracias a la financiación de
-<a href="https://www.fondationcarasso.org/es/">
+Hecha con ♥️ por <a target="_blank" href="https://github.com/danigb">danigb</a>
+y <a target="_blank" href="https://github.com/mi-mina">mi-mina</a>
+</p>
+<p>
+Y la financiación y apoyo   de
+<a target="_blank" href="https://www.fondationcarasso.org/es/">
 <img src="/carasso.jpg" alt="Fundación Caraso">
 </a>
 </p>
 <p>¡Muchas gracias!</p>
 
+<br>
+<br>
 <p>
-Tanto el
-<a href="https://github.com/antropoloops/player">
+El
+<a target="_blank" href="https://github.com/antropoloops/player">
 código fuente
 </a>
- como los contenidos están publicados con licencias libres.
+ está publicado con licencia de código libre.
+</p>
+<br>
+
+<p>
+Para cualquier propuesta o comentario escríbenos a
+<a href="mailto:talleres@antropoloops.com">talleres@antropoloops.com</a>
+o
+<a target="_blank" href="https://twitter.com/antropoloopsEDU">https://twitter.com/antropoloopsEDU</a>
 </p>
 `;
 
