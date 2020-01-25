d9ae4d6640d6c94146d5d49449d90e2c9a8a6a44
danigb <danigb@gmail.com>
2020-01-25 19:46:12 +0100

    Move decodeAudioBuffer

--- src/player/Audio/index.ts
+++ src/player/Audio/index.ts
@@ -1,5 +1,4 @@
 export { getActiveAudioContext, autoUnlockAudio } from "./AudioContext";
-export { decodeAudioBuffer } from "./decodeAudioBuffer";
 export * from "./Sampler";
 export * from "./AudioContextEngine";
 export * from "./TimeManager";
