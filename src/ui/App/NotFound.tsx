d6e35864e61b2926c98241f7a0d054f76ebb46da
danigb <danigb@gmail.com>
2020-03-23 23:57:47 +0100

    Change background image. Fixes #56

--- src/ui/App/NotFound.tsx
+++ src/ui/App/NotFound.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useAnalytics from "../hooks/useAnalytics";
 import { Header } from "../shared/Header";
 
 const NotFound = () => {
+  useAnalytics();
   return (
     <div className="App NotFound">
       <Header />
       <div className="content">
         <div className="inside">
-          <img src="/lik.jpg" alt="Where to find" />
+          <img src="/notfound.jpg" alt="Where to find" />
           <p>Lo sentimos, no hemos encontrado lo que buscabas.</p>
           <p>
             Si crees que algo va mal, por favor escríbenos a
