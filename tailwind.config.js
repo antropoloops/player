8f5c3db1501ff5dabcbe9b7e7e0945f048fb34f8
danigb <danigb@gmail.com>
2020-09-20 11:29:49 +0200

    Fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  purge: ["./src/**/*.tsx"],
+  purge: {
+    content: ["./src/**/*.tsx"],
+    options: {
+      whitelist: [
+        "grid-cols-1",
+        "grid-cols-2",
+        "grid-cols-3",
+        "grid-cols-4",
+        "grid-cols-5",
+        "grid-cols-6",
+        "grid-cols-7",
+        "grid-cols-1",
+        "grid-cols-9",
+        "grid-cols-10",
+        "grid-cols-11",
+        "grid-cols-12",
+      ],
+    },
+  },
   theme: {
     extend: {
       colors: {
