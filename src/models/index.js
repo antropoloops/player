b1c63ce2e891543050a020e62217bca54abc8c00
danigb <danigb@gmail.com>
2020-12-17 18:22:13 +0100

    Amplify setup movements

--- src/models/index.js
+++ src/models/index.js
@@ -2,33 +2,10 @@
 import { initSchema } from '@aws-amplify/datastore';
 import { schema } from './schema';
 
-const ImageUnits = {
-  "PX": "px",
-  "PERCENT": "percent"
-};
 
-const ArchiveAccess = {
-  "PRIVATE": "private",
-  "PUBLIC": "public"
-};
 
-const { Group, Archive, Recording, Sample, Remix, Track, StoredImage, ImageCrop, StoredAudio, RecordingMeta, SampleRegion, RemixMetadata, TrackMetadata, ClipMetadata } = initSchema(schema);
+const { Group } = initSchema(schema);
 
 export {
-  Group,
-  Archive,
-  Recording,
-  Sample,
-  Remix,
-  Track,
-  ImageUnits,
-  ArchiveAccess,
-  StoredImage,
-  ImageCrop,
-  StoredAudio,
-  RecordingMeta,
-  SampleRegion,
-  RemixMetadata,
-  TrackMetadata,
-  ClipMetadata
+  Group
 };
